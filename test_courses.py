fabaf95649a5310168e1622cb41b88b0565ee247
Your Name <you@example.com>
2025-11-11 17:09:12 +0300

    add  test_courses.py

--- test_courses.py
+++ test_courses.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import sync_playwright, expect
 
-def test_courses():
+def test_empty_courses_list():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
         context = browser.new_context()
