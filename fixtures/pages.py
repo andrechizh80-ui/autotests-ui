f9710b0b0c325129f730c96a5059a27fa981f50a
Your Name <you@example.com>
2025-11-12 21:05:58 +0300

    add test

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -1,5 +1,7 @@
 from pages.registration_page import RegistrationPage
 from pages.dashboard_page import DashboardPage
+from pages.create_course_page import CreateCoursePage
+from pages.courses_list_page import CoursesListPage
 import pytest
 from playwright.sync_api import Page
 
@@ -12,3 +14,11 @@ def registration_page(chromium_page: Page) -> RegistrationPage:
 @pytest.fixture
 def dashboard_page(chromium_page: Page) -> DashboardPage:
     return DashboardPage(chromium_page)
+
+@pytest.fixture
+def create_course_page(chromium_page_with_state: Page) -> CreateCoursePage:
+    return CreateCoursePage(page=chromium_page_with_state)
+
+@pytest.fixture
+def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
+    return CoursesListPage(page=chromium_page_with_state)
