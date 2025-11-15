55296ee28ef5948f9f63b290ed3277c72ffdb1bc
Your Name <you@example.com>
2025-11-16 01:00:03 +0300

    add allure

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -1,17 +1,34 @@
+import allure
 import pytest
 from pages.courses.create_course_page import CreateCoursePage
 from pages.courses.courses_list_page import CoursesListPage
+from tools.allure.epic import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
+from allure_commons.types import Severity
 
 
 @pytest.mark.courses
 @pytest.mark.regression
+@allure.tag(AllureTag.REGRESSION, AllureTag.COURSES)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.COURSES)
+@allure.story(AllureStory.COURSES)
+@allure.suite(AllureFeature.COURSES)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.sub_suite(AllureStory.COURSES)
 class TestCourses:
+    @allure.title("Check displaying of empty courses list")
+    @allure.severity(Severity.NORMAL)
     def test_empty_courses_list(self, courses_list_page: CoursesListPage):
         courses_list_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
         courses_list_page.navbar.check_visible("username")
         courses_list_page.sidebar.check_visible()
         courses_list_page.check_visible_empty_view()
 
+    @allure.title("Create course")
+    @allure.severity(Severity.CRITICAL)
     def test_create_course(self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
         create_course_page.visit(
             url='https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
@@ -46,6 +63,8 @@ class TestCourses:
             min_score="100"
         )
 
+    @allure.title("Edit course")
+    @allure.severity(Severity.CRITICAL)
     def test_edit_course(self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
         create_course_page.visit(
             url='https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
